94eef2e319cd5a51ba893f0416f3fcd6e7d6c03a
Geoffroy <geoffroy.blondel@gmail.com>
2021-03-31 07:56:21 +0200

    update tor client

--- python/core.py
+++ python/core.py
@@ -1,11 +1,5 @@
 import time
-
 import requests
-from fake_useragent import UserAgent
-from rest_framework import status
-from rest_framework.response import Response
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from stem import Signal
 from stem.control import Controller
 from pythonping import ping
@@ -13,38 +7,13 @@ import re
 
 
 class TorClient:
-    options = webdriver.ChromeOptions()
-
-    ua = UserAgent(use_cache_server=False, verify_ssl=False)
-    # headers = requests.utils.default_headers()
-
     def __init__(self):
         self.PROXY = f"socks5://{self.get_tor_container_ip()}:9050"
-        self.options.add_argument('--proxy-server=%s' % self.PROXY)
-        #self.options.add_argument('--no-sandbox')
-        self.options.add_argument(f'user-agent={self._get_ua()}')
-        self.driver = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME, options=self.options)
-        self.driver.set_window_position(1024, 1024, windowHandle='current')
-        self.driver.set_window_size(1920, 1080)
 
     def get(self, url):
         self.rotate()
-        response = Response()
-
-        try:
-            self.driver.get(url)
-            time.sleep(5)
-            response.status_code = status.HTTP_200_OK
-            response.content = self.driver.page_source
-
-        except Exception as e:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            response.content = str(e)
-
+        response = requests.get(url)
         return response
-
-    def _get_ua(self):
-        return self.ua.random
 
     def get_public_ip(self):
         proxies = {
@@ -58,6 +27,7 @@ class TorClient:
         return re.findall(r'[0-9]+(?:\.[0-9]+){3}', str(ping("tor")))[0]
 
     def rotate(self):
+        # Rotate tor public ip
         with Controller.from_port(address=f"{self.get_tor_container_ip()}", port=9051) as controller:
             controller.authenticate(password="bonjour")
             controller.signal(Signal.NEWNYM)
